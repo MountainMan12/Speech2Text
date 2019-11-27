753414e415e38efa65b7aed581a938974716d122
MountainMan <46047927+MountainMan12@users.noreply.github.com>
2019-11-28 01:20:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 # This setup is suitable for "python setup.py develop".
 
-setup(name='Speech2Text',
+setup(name='SpeechProcessing',
       version='0.1',
       description='Python Library to convert a speech text to a written text',
       author='Pawan Verma',
       author_email='pawan12394@gmail.com',
       url='https://github.com/MountainMan12/Speech2Text',
-      packages=['Speech2Text'],
+      packages=['SpeechProcessing'],
       )
