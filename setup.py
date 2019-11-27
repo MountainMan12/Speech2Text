4b916b9ef90b27ca37e9118727907a3d4123aea2
MountainMan <46047927+MountainMan12@users.noreply.github.com>
2019-11-27 23:13:12 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+
+# This setup is suitable for "python setup.py develop".
+
+setup(name='Speech2Text',
+      version='0.1',
+      description='Python Library to convert a speech text to a written text',
+      author='Pawan Verma',
+      author_email='pawan12394@gmail.com',
+      url='https://github.com/MountainMan12/Speech2Text',
+      packages=['Speech2Text'],
+      )
